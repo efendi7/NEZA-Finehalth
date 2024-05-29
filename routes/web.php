50cdd00f95b61efcf4fb078d3f6962fd9fca561e
Abi Kurniawan <abikurniawan154@gmail.com>
2024-05-29 14:47:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/buku', function () {
+    return view('Buku');
+});
+
+Route::get('/dompet', function () {
+    return view('Dompet');
+});
+
+Route::get('/analisis', function () {
+    return view('Analisis');
+});
+
+Route::get('/more', function () {
+    return view('More');
 });
