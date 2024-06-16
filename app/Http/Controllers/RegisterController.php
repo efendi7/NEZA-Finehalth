48423ad3b7800154eab5804d31ba56a7aec7dff1
unknown <zbanjar9@gmail.com>
2024-06-16 22:30:40 +0700

    membuat fungsi sign up untuk menyimpan user ke dalam database

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,11 +12,14 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
-        $request->validate([
+        $validatedData = $request->validate([
             'name'=>'required|max:255|unique:users',
             'email'=>'required|email:dns|unique:users',
             'password'=>'required|min:8|max:255'
         ]);
         
+        User::create($validatedData);
+
+        return redirect('/buku');
     }
 }
